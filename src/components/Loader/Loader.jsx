12ead642646bf7cb481bcfac4146543f218f0552
kika88vk <69463446+kika88vk@users.noreply.github.com>
2023-12-01 12:56:22 +0200

    add loader and github deploy

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { Rings } from "react-loader-spinner";
 
 const Loader = () => {
-  return <div>Loader</div>;
+  return (
+    <div>
+      <Rings
+        height="80"
+        width="80"
+        color="#3470FF"
+        radius="6"
+        wrapperStyle={{}}
+        wrapperClass=""
+        visible={true}
+        ariaLabel="rings-loading"
+      />
+    </div>
+  );
 };
 
 export default Loader;
