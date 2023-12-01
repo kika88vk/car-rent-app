afa1041b174e96d72987c4bdb571dd3bc51be0ff
kika88vk <69463446+kika88vk@users.noreply.github.com>
2023-12-02 01:20:47 +0200

    add styles item

--- src/components/CarListItem/CarListItem.jsx
+++ src/components/CarListItem/CarListItem.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import sprite from "../../assets/sprite.svg";
+import css from "./CarListItem.module.css";
 
 const CarListItem = ({
   idCar,
@@ -20,20 +21,28 @@ const CarListItem = ({
   mileage,
 }) => {
   return (
-    <li>
-      <img src={image} alt={model} />
-      <svg>
+    <li className={css.listItem}>
+      <img src={image} alt={model} className={css.carImg} />
+      <svg className={css.icon}>
         <use href={`${sprite}#icon-heart-normal`} />
       </svg>
-      <p>{make}</p>
-      <p>{model},</p>
-      <p>{year}</p>
-      <p>{rentalPrice}</p>
-      <span>{address}</span>
-      <span>{rentalCompany}</span>
-      <span>{type}</span>
-      <span>{idCar}</span>
-      <span>{functionalities}</span>
+      <div className={css.wrapName}>
+        <div className={css.carName}>
+          <p>{make}</p>
+          <p className={css.accent}>{model},</p>
+          <p>{year}</p>
+        </div>
+        <p className={css.price}>{rentalPrice}</p>
+      </div>
+
+      <div className={css.descrText}>
+        <span>{address}</span>
+        <span>{rentalCompany}</span>
+        <span>{type}</span>
+        <span>{idCar}</span>
+        <span>{functionalities}</span>
+      </div>
+
       <button type="button">Learn more</button>
     </li>
   );
